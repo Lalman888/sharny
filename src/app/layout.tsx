ba2810895244489a6957b937607a297d40e9a3a2
Lalman <chauhanlakshay074@gmail.com>
2023-06-19 21:34:24 +0530

    feat: setup husky

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
-import '@/styles/globals.css'
+import { cn } from "@/lib/utils"
+
+import "@/styles/globals.css"
+
+import { Inter } from "next/font/google"
 
 export const metadata = {
-  title: 'Sharny',
-  description: 'On Sharny you can share your thoughts with the world.',
+  title: "Sharny",
+  description: "On Sharny you can share your thoughts with the world.",
 }
+
+const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({
   children,
@@ -11,8 +17,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "text-slate-900 bg-white antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased ">
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
